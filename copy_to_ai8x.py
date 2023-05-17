68eed4b7efac083817cc6d54a441fb5911242ddf
lkesting <lkesting@student.ethz.ch>
2023-05-17 13:36:07 +0200

    Script to update files in ai8x directories

---
+++ copy_to_ai8x.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copy widerfacenet.yaml to /maxim7800-face-detection/ai8x/Exercise8/ai8x-synthesis/networks
+cp ./ai8x/widerfacenet.yaml ../Exercise8/ai8x-synthesis/networks/
+
+# Copy wider_face_net.py to /Exercise8/ai8x-training/model
+cp ./ai8x/wider_face_net.py ../Exercise8/ai8x-training/models/
+
+# Copy wider_faces.py to /Exercise8/ai8x-training/dataset
+cp ./ai8x/wider_faces.py ../Exercise8/ai8x-training/datasets/
+
+# Copy qat_policy_widerfacenet.yaml to /Exercise8/ai8x-training/policies
+cp ./ai8x/qat_policy_widerfacenet.yaml ../Exercise8/ai8x-training/policies/
