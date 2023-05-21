bebc062a30aaa20c1cd547b2db623e23cbd66822
lkesting <lkesting@student.ethz.ch>
2023-05-21 23:47:19 +0200

    Script to copy files in correct locatoin for ai8x-training

--- copy_to_ai8x.sh
+++ copy_to_ai8x.sh
@@ -4,10 +4,10 @@
 cp ./ai8x/widerfacenet.yaml ../Exercise8/ai8x-synthesis/networks/
 
 # Copy wider_face_net.py to /Exercise8/ai8x-training/model
-cp ./ai8x/wider_face_net.py ../Exercise8/ai8x-training/models/
+cp ./ai8x/widerfacenet.py ../Exercise8/ai8x-training/models/
 
 # Copy wider_faces.py to /Exercise8/ai8x-training/dataset
-cp ./ai8x/wider_faces.py ../Exercise8/ai8x-training/datasets/
+cp ./ai8x/widerfaces.py ../Exercise8/ai8x-training/datasets/
 
 # Copy qat_policy_widerfacenet.yaml to /Exercise8/ai8x-training/policies
 cp ./ai8x/qat_policy_widerfacenet.yaml ../Exercise8/ai8x-training/policies/
