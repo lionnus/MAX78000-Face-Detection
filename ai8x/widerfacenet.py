76118a689baf5cb7156a5b8db357715828f26e2f
lkesting <lkesting@student.ethz.ch>
2023-05-22 22:11:17 +0200

    Changed to working test structure

--- ai8x/widerfacenet.py
+++ ai8x/widerfacenet.py
@@ -111,46 +111,36 @@ import matplotlib.pyplot as plt
 
 #             return x
 class WIDERFaceNet(nn.Module):
-    def __init__(self, num_classes=10, dimensions=(128, 128), num_channels=1, bias=False, **kwargs):
+    def __init__(self, num_channels=3, dimensions = (64,64), bias=False, **kwargs):
         super().__init__()
+        self.conv1 = ai8x.FusedConv2dReLU(3, 4, kernel_size=3, padding=1)
+        self.pool1 = ai8x.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU()
+        self.conv2 = ai8x.FusedConv2dReLU(4, 8, kernel_size=3, padding=1)
+        self.pool2 = ai8x.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
+        self.conv3 = ai8x.FusedConv2dReLU(8, 16, kernel_size=3, padding=1)
+        self.pool3 = ai8x.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.relu3 = nn.ReLU()
+        self.fc1 = ai8x.Linear(16 * 8*8, 32)
 
-        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
-        self.bn4 = nn.BatchNorm2d(512)
-        self.relu4 = nn.ReLU()
-        num_bboxes = 1 # Harcoded for now
-        self.fcx = nn.Linear(16*16*3, num_bboxes, bias=True)
+        self.fc2 = ai8x.Linear(32, 4)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
+        x = self.pool1(x)
 
         x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
+        x = self.pool2(x)
 
         x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
-
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = self.relu4(x)
+        x = self.pool3(x)
 
         x = x.view(x.size(0), -1)
-        x = self.fcx(x)
+
+        x = self.fc1(x)
+
+        x = self.fc2(x)
 
         return x
 
