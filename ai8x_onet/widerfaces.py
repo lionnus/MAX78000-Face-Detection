3f0b44b3b2e3ad6ecfe4fe1e3f7c3234b42f3c3a
lkesting <lkesting@student.ethz.ch>
2023-06-02 16:35:47 +0200

    Dataloader that crops pictures small faces

--- ai8x_onet/widerfaces.py
+++ ai8x_onet/widerfaces.py
@@ -14,6 +14,7 @@ import torchvision
 from torchvision import transforms
 from torch.utils.data import Dataset
 import torch
+import random
 
 import ai8x
 
@@ -25,6 +26,15 @@ class WIDERFacesDataset(Dataset):
         self.data_path = data_path
         self.annotation_file = os.path.join(self.data_path, "wider_face_bbx_gt_no_faces.txt")
         self.transform = transform
+
+        self.flags = {	
+                    # 'blur': 0,	
+                    # 'expression': 0,	
+                    # 'illumination': 0,	
+                    # 'invalid': 0,	
+                    'occlusion': 0,	
+                    'pose': 0	
+                }
         
         # Load data and annotations
         self.data, self.annotations = self.load_data()
@@ -39,6 +49,8 @@ class WIDERFacesDataset(Dataset):
         
         bboxes = self.annotations[idx]['bboxes']
         face_label = self.annotations[idx]['labels']['faces']
+        # Resize image and bbox if the face is too small	
+        image, bboxes = self.square_crop_around_bbox(image, bboxes[0])
 
         # Resize image to 48 x 48 and update boundary boxes accordingly
         bboxes_resized = self.resize_bbox(bboxes, image.shape[1], image.shape[0], 48,48)
@@ -112,13 +124,54 @@ class WIDERFacesDataset(Dataset):
                     'bboxes': bboxes,
                     'labels': label
                 }
-                if(num_bboxes ==1 or num_bboxes == 0):
-                    annotations.append(annotation)
-                    data.append(image_file_path)
+                 # Filter for images with only a single face	
+                if(num_bboxes ==1 or num_bboxes==0):	
+                    # Filter out images with have certain unusual flags	
+                    if all(label[key] == 0 for key in self.flags.keys()):	
+                        annotations.append(annotation)	
+                        data.append(image_file_path)
                 i += 1
 
         return data, annotations
-    
+    def square_crop_around_bbox(self, image, bbox, min_ratio=0.15, max_ratio=0.8):	
+        image_height, image_width, _ = image.shape	
+        bbox_x = bbox[0]	
+        bbox_y = bbox[1]	
+        bbox_width = bbox[2]	
+        bbox_height = bbox[3]	
+        # Dont do anything if the bbox is empty	
+        if (bbox_width == 0 or bbox_height == 0):	
+            return image, [bbox]	
+            
+        # Calculate the ratio of bbox width or height to the smallest image dimension	
+        ratio = min(bbox_width / image_width, bbox_height / image_height)	
+            
+        # print("Image Dimensions: ", image_width, "x", image_height)	
+        # print("Bounding Box Dimensions: ", bbox_width, "x", bbox_height)	
+        # print("Bounding Box Ratio: ", ratio)	
+        # Check if the ratio is smaller than the threshold	
+        # if ratio < min_ratio:	
+        #     print("Found image with ratio smaller than the minimum threshold: ", ratio)	
+        proposed_width = random.randint(int(bbox_width / max_ratio), int(bbox_width / min_ratio))	
+        proposed_height = random.randint(int(bbox_height / max_ratio), int(bbox_height / min_ratio))	
+        # Make images square whilst we're at it	
+        min_new_dim = min(proposed_width, proposed_height)	
+        new_width = min_new_dim if min_new_dim > bbox_width else proposed_width	
+        new_height = min_new_dim if min_new_dim > bbox_height else proposed_height	
+        # Calculate the offsets	
+        # print("Min x_offset: ", max(0,bbox_x+bbox_width-new_width), "Max x_offset: ", bbox_x-1)	
+        x_offset = random.randint(max(0,bbox_x+bbox_width-new_width), bbox_x)	
+        # print("Min y_offset: ", max(0,bbox_y+bbox_height-new_height), "Max y_offset: ", bbox_y-1)	
+        y_offset = random.randint(max(0,bbox_y+bbox_height-new_height), bbox_y)	
+        # print("Offsets: ", x_offset, y_offset)	
+        # Perform the cropping operation	
+        cropped_image = image[y_offset:y_offset + new_height, x_offset:x_offset + new_width]	
+        bbox[0] -= x_offset	
+        bbox[1] -= y_offset	
+            
+        # print("New minimum ratio: ", min(bbox_width / new_width, bbox_height / new_height))	
+        return cropped_image, [bbox]
+
     def resize_bbox(self,bboxes, dim_x_init,dim_y_init, dim_x,dim_y):
         bboxes_resized = []
         #print(bboxes, dim_x_init,dim_y_init, dim_x,dim_y)
