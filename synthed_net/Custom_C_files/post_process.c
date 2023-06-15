bd77f2cb1df23a57282c7806220d33d3a87d2f8b
lkesting <lkesting@student.ethz.ch>
2023-06-15 16:16:06 +0200

    Fixed Q17.14 to float conversion

--- synthed_net/Custom_C_files/post_process.c
+++ synthed_net/Custom_C_files/post_process.c
@@ -10,34 +10,29 @@
 
 void convert_to_bbox(uint32_t *ml_data, float *bbox){
 	for (int i=0; i<4; i++){
-		bbox[i] = q17_14_to_float(ml_data[i]);
+		bbox[i] = (q17_14_to_float(ml_data[i])+1)*24;
 	}
 }
 
 float q17_14_to_float(uint32_t q_value) {
-        // Extract the sign bit
-   uint32_t sign = (q_value >> 31) & 0x1;
+      // Extract the sign bit
+      uint32_t sign = (q_value >> 31) & 0x1;
+      if (sign) {
+    	  q_value = (~q_value)+1;
+      }
 
-   if (sign==1)
-    q_value = (~q_value) + 1;
+      // Extract the integer part
+      uint32_t integer_part = (q_value >> 14) & 0x1FFFF;
 
-        // Extract the integer part
-     uint32_t integer_part = (q_value >> 14) & 0x1FFFF;
+      // Extract the fractional part
+      uint32_t fractional_part = q_value & 0x3FFF;
 
-        // Extract the fractional part
-     uint32_t fractional_part = q_value & 0x3FFF;
+      // Calculate the float value
+      float float_value = (float)integer_part + (float)fractional_part / (float)(1<<14);
 
-        // Calculate the float value
-        //float float_value = (float)integer_part + (float)fractional_part / 16384.0f; //16384.0f = 2**14
-     float float_value = (float)fractional_part / 16384.0f;
+      // Apply the sign
+      if (sign)
+          float_value *= -1.0f;
 
-
-        // Apply the sign
-        if (sign==1){
-            float_value *= -1.0f;
-        }
-
-        //printf("%f", float_value);
-
-        return float_value;
-}
+      return float_value;
+  }
